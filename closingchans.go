1b767e7a8b91353c2aff362ced6890c00c042cc2
Shrayas <shrayasr@gmail.com>
2014-03-02 18:07:30 +0530

    Modularizing the go program
    I dont like anon fxns yet

--- closingchans.go
+++ closingchans.go
@@ -2,25 +2,25 @@ package main
 
 import "fmt"
 
+func worker(jobs chan int, done chan bool) {
+  for {
+    j, more := <-jobs
+    if more {
+      fmt.Println("Job received, ",j)
+    } else {
+      fmt.Println("No more jobs")
+      done <- true
+      return
+    }
+  }
+}
+
 func main() {
 
   jobs := make(chan int, 5)
   done := make(chan bool)
 
-  go func(){
-
-    for {
-      j, more := <-jobs
-      if more {
-        fmt.Println("Job received, ",j)
-      } else {
-        fmt.Println("No more jobs")
-        done <- true
-        return
-      }
-    }
-
-  }()
+  go worker(jobs, done)
 
   for j:=0; j<3; j++ {
     jobs <- j
